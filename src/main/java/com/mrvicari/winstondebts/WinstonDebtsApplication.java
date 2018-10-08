ffdc7db2ef2209378537cce1da8506b132caf3ff
Miguel Vicari <mvicari@caci.co.uk>
2018-10-08 15:50:42 +0100

    #1 add checkstyle

--- src/main/java/com/mrvicari/winstondebts/WinstonDebtsApplication.java
+++ src/main/java/com/mrvicari/winstondebts/WinstonDebtsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WinstonDebtsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WinstonDebtsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WinstonDebtsApplication.class, args);
+    }
 }
--- src/main/java/com/mrvicari/winstondebts/controller/WelcomeController.java
+++ src/main/java/com/mrvicari/winstondebts/controller/WelcomeController.java
@@ -1,8 +1,7 @@
 package com.mrvicari.winstondebts.controller;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
